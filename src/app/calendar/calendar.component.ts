fbeaf52f64d366e1b3d923d2f46eafe601c53856
NMakarevich <nikoladze529@mail.ru>
2022-11-30 22:01:07 +0300

    feat: add select for month and year

--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -20,4 +20,49 @@ export class CalendarComponent {
   get prevMonthS() {
     return this.dateService.prevMonth();
   }
+
+  get stringMonth() {
+    return this.monthList[this.currentMonth.getMonth()];
+  }
+
+  get stringYear() {
+    return this.dateService.date.getFullYear().toString();
+  }
+
+  setMonth = (value: string) => {
+    const month = this.monthList.indexOf(value as string);
+    const year = this.dateService.date.getFullYear();
+    this.dateService.updateDate(new Date(year, month, 1));
+  };
+
+  setYear = (value: string) => {
+    const month = this.dateService.date.getMonth();
+    const year = parseInt(value);
+    this.dateService.updateDate(new Date(year, month, 1));
+  };
+
+  get monthList() {
+    return [
+      'January',
+      'February',
+      'March',
+      'April',
+      'May',
+      'June',
+      'July',
+      'August',
+      'September',
+      'October',
+      'November',
+      'December',
+    ];
+  }
+
+  get years() {
+    const yearsArr = [];
+    for (let i = 1970; i <= 2030; i += 1) {
+      yearsArr.push(i.toString());
+    }
+    return yearsArr;
+  }
 }
--- src/app/calendar/calendar.module.ts
+++ src/app/calendar/calendar.module.ts
@@ -5,10 +5,11 @@ import { MonthModule } from './month/month.module';
 import { DayModule } from './day/day.module';
 import { DateService } from './date.service';
 import { SliderModule } from './slider/slider.module';
+import { SelectModule } from './select/select.module';
 
 @NgModule({
   declarations: [CalendarComponent],
-  imports: [CommonModule, MonthModule, DayModule, SliderModule],
+  imports: [CommonModule, MonthModule, DayModule, SliderModule, SelectModule],
   providers: [DateService],
   exports: [CalendarComponent],
 })
